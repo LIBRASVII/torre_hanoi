b18931f79e6f6057e2a3b098104df8d33dcb762e
LIBRASVII <moreiralucas511@gmail.com>
2023-09-16 10:36:11 -0300

    added function

--- torre_hanoi.c
+++ torre_hanoi.c
@@ -1,3 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() { return 0; }
+typedef struct hanoi {
+  int cap;
+  char torre_A, torre_B, torre_C;
+  char *prox;
+  int top;
+} Hanoi;
+
+void cria_torre(Hanoi *, int);
+void push_A(Hanoi *, char);
+void hanoi(Hanoi *, char, char, char);
+
+int main() {
+  Hanoi hanoi;
+  int cap;
+
+  printf("\nCap torre ==> ");
+  scanf("%d", &cap);
+
+  cria_torre(&hanoi, cap);
+
+  return 0;
+}
+
+void hanoi(Hanoi *h, char a, char b, char c) {}
+
+void cria_torre(Hanoi *h, int c) {
+  h->prox = (char *)malloc(c * sizeof(int));
+  h->top = -1;
+  h->cap = c;
+}
+
+void push_A(Hanoi *h, char v) {
+  h->torre_A = v;
+  h->top = v;
+  h->top++;
+}
